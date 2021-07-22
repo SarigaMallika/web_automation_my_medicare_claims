e08f9dbcb6c9db60c2e6523a4123dd1000f23a70
Sariga Sajeev <53913474+SarigaMallika@users.noreply.github.com>
2021-07-22 15:09:48 +0530

    Add files via upload

---
+++ Runner_Files/My_medicare_claims_positive_scenario.java
@@ -0,0 +1,14 @@
+package Runner_Files;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+		features = "src\\main\\java\\My_medicare_claims_positive_scenario\\My_medicare_claims_positive_scenario.feature",
+		glue= {"My_medicare_claims_positive_scenario"},
+		monochrome = true,publish = true)
+public class My_medicare_claims_positive_scenario extends AbstractTestNGCucumberTests{
+
+}
+
