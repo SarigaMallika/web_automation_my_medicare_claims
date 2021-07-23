c442724714b95248aa79033798ae66bf977b2b4f
Sariga Sajeev <53913474+SarigaMallika@users.noreply.github.com>
2021-07-23 15:56:14 +0530

    Update My_medicare_claims_positive_scenario.java

--- Runner_Files/My_medicare_claims_positive_scenario.java
+++ Runner_Files/My_medicare_claims_positive_scenario.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","pretty","html:target/html", "json:target/cucumber.json"}},
+		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 		features = "src\\main\\java\\My_medicare_claims_positive_scenario\\My_medicare_claims_positive_scenario.feature",
 		glue= {"My_medicare_claims_positive_scenario"},
 		monochrome = true,publish = true)
